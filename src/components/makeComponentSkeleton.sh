3bf2921f450e477ed0711c52f253a2a524b58cfd
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-24 18:50:40 -0700

    convenience script to generate component boilerplate

---
+++ src/components/makeComponentSkeleton.sh
@@ -0,0 +1,10 @@
+#/!bin/bash
+
+read -p ' Enter component name ' name
+mkdir ./$name
+echo 'Touching' $name'.js'
+touch ./$name/$name.js
+echo 'Touching' $name'.css'
+touch ./$name/$name.css
+echo 'Touching' $name'.test.js'
+touch ./$name/$name.test.js
