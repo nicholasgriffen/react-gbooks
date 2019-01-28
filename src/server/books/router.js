32847f6044a9a4d8054f6e9e63436a98e7bba404
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-28 10:43:36 -0700

    extract schema, green test /books return type and code

--- src/server/books/router.js
+++ src/server/books/router.js
@@ -1,8 +1,15 @@
 const { Router } = require('express')
+
+const { search } = require('../lib/googleBooksClient')
+
 const router = Router()
 
-router.use('/', (req, res, next) => {
-    res.sendStatus(200)
+router.use('/', async (req, res, next) => {
+    const books = await search().catch(e => {
+        return next(e)
+    })
+    res.status = 200
+    res.json(books)
 })
 
 module.exports = router
--- src/server/test/booksRouter.test.js
+++ src/server/test/booksRouter.test.js
@@ -1,16 +1,19 @@
 const chai = require('chai')
 const { expect } = chai
+
 const app = require('../index')
+const schema = require('./googleBooksSchema')
 
 chai.use(require('chai-http'))
 
 describe('/books', () => {
-    it('responds to a get with status 200', () => {
+    it('responds to a get with status 200 and JSON object matching the schema', () => {
         return chai
             .request(app)
             .get('/books')
             .then(res => {
                 expect(res.status).to.equal(200)
+                expect(res.body).to.be.an('object').that.has.all.deep.keys(schema)
             })
     })
 })
