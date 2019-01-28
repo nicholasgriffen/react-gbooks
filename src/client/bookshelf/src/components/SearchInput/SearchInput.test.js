60b9a785579087a7cd27648a1a4eac71786997fb
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-28 12:43:36 -0700

    fix component import, define first test

--- src/client/bookshelf/src/components/SearchInput/SearchInput.test.js
+++ src/client/bookshelf/src/components/SearchInput/SearchInput.test.js
@@ -4,10 +4,21 @@ import Adapter from 'enzyme-adapter-react-16'
 import chai from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 
-import Footer from './Footer'
+import SearchInput from './SearchInput'
 
 Enzyme.configure({ adapter: new Adapter() })
 chai.use(chaiEnzyme())
 
 const shallow = Enzyme.shallow
 const { expect } = chai 
+
+describe('<SearchInput />', () => {
+    it('renders an input with type search', () => {
+        const wrapper = shallow(<SearchInput />)
+        const input = wrapper.find('input')
+        const type = input.prop('type')
+        
+        expect(input).to.have.lengthOf(1)
+        expect(type).to.equal('search')
+    })
+})
