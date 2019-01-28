26b4ea64aa70150b0ef55f84a812289d35d44af9
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-28 12:46:26 -0700

    render search input

---
+++ src/client/bookshelf/src/components/SearchInput/SearchInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class SearchInput extends React.Component {
+    render() {
+        return (
+            <input type="search"></input>
+        )
+    }
+}
