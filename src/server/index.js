13930d9064258b225a85fc90ca7d60a34eb101ae
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-28 16:43:13 -0700

    fix listen func and error handler

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,24 @@
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
 
 const booksRouter = require('./books/router')
-require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT
 
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/books', booksRouter)
 
 
-app.use((req, res, next, err) => {
+app.use((err, req, res, next) => {
     res.status = err.status ? err.status : 500
-    
-    if (process.env.NODE_ENV !== 'production') {
-        res.send(err)
-    } else {
-        const safeError = new Error("Something's gone awry, please visit your local library for now")
+    const safeError = new Error("Something's gone awry, please visit your local library for now")
         res.send(safeError)
-    }
-    
 }) 
 
-app.listen(() => console.log(`listening on ${PORT}`))
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
 
 module.exports = app
