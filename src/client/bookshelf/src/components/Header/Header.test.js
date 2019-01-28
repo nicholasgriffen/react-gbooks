bd18f0aa340568dfc8de85570eb4d3d089f4cde7
Nicholas <nicholas.s.griffen@gmail.com>
2019-01-28 12:19:15 -0700

    define header tests for expected elements

--- src/client/bookshelf/src/components/Header/Header.test.js
+++ src/client/bookshelf/src/components/Header/Header.test.js
@@ -1,4 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+
 import Header from './'
 
+describe('<Header />', () => {
+    it('renders a #logo', () => {
+        const wrapper = shallow(<Header />)
+        expect(wrapper.find('#logo')).to.have.lengthOf(1)
+    })
+    it('renders a #headerTitle', () => {
+        const wrapper = shallow(<Header />)
+        expect(wrapper.find('#headerTitle')).to.have.lengthOf(1)        
+    })
+    it('renders a header', () => {
+        const wrapper = shallow(<Header />)
+        expect(wrapper.find('header')).to.have.lengthOf(1)        
+    })
+})
