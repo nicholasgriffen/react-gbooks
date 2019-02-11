76d4599216a8b8b78d035d0746d2a74bfa50860e
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-02-11 10:58:40 -0700

    eject from cra, begin removing unused code and deps

--- src/client/src/components/App/App.js
+++ src/client/src/components/App/App.js
@@ -3,22 +3,20 @@ import Header from '../Header/Header'
 import SearchInput from '../SearchInput/SearchInput'
 import Books from '../Books/Books'
 
-import './App.css'
-
 class App extends Component {
 
   constructor() {
     super()
     this.state = {
       books: [],
-      endpoint: process.env.REACT_APP_API_ENDPOINT, 
+      endpoint: process.env.REACT_APP_API_ENDPOINT,
     }
   }
-  
-  // running searches with a debounced onChange would be a nice UX 
-  // but had difficulty getting that to work in a reasonable way - typically 
-  // triggered many requests, and race conditions between render, setState, and 
-  // the mapping in the Books component 
+
+  // running searches with a debounced onChange would be a nice UX
+  // but had difficulty getting that to work in a reasonable way - typically
+  // triggered many requests, and race conditions between render, setState, and
+  // the mapping in the Books component
 
   onSearchSubmit = term => {
     return fetch(`${this.state.endpoint}/?search=${encodeURIComponent(term)}`).then(res => {
@@ -26,8 +24,8 @@ class App extends Component {
    }).then(json => {
      const books = json.items
      return this.setState({
-      ...this.state, 
-      books 
+      ...this.state,
+      books
     })
    })
   }
@@ -36,14 +34,14 @@ class App extends Component {
     return (
       <div id='react-app'
       style={{
-        display: "flex", 
+        display: "flex",
         "flex-direction": "column",
-        "justify-content": "space-between" 
+        "justify-content": "space-between"
         }}>
         <Header/>
         <SearchInput
          onSubmit={this.onSearchSubmit}/>
-        <Books 
+        <Books
          books={this.state.books}/>
       </div>
     )
