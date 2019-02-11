333b3dc3b5112130662890a6bb02b7ca49fe590a
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-02-11 09:52:11 -0700

    remove folder nesting, build artifacts

--- src/server/index.js
+++ src/server/index.js
@@ -15,8 +15,10 @@ app.use('/books', booksRouter)
 
 app.use((err, req, res, next) => {
     res.status = err.status ? err.status : 500
+    
     const safeError = new Error("Something's gone awry, please visit your local library for now")
-        res.send(safeError)
+    
+    res.send(safeError)
 }) 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
