dcb9e0c58ca1691d97dcf6cf6f65a6abeb4678e0
Nick Griffen <nicholas.s.griffen@gmail.com>
2019-02-11 17:39:54 -0700

    add comments, update actions

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #/!bin/bash
 
+# remove existing public dir
+rm -rf ./src/server/public
+
+# move into client dir
 cd src/client
+# run modified create-react-app build script
 node scripts/build.js
+
+# move back to project top level
 cd ../..
-mv ./src/client/build ./src/server/public
+
+# move build Client app to Server public dir
+mv ./src/client/build ./src/server/public 
+
+# copy index.css and favicon to Server public dir
+cp ./src/client/public/index.css ./src/server/public/.
+cp ./src/client/public/favicon.ico ./src/server/public/.
